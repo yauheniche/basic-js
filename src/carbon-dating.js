cb410ce1302f74c66f0bd49d579e00b000680fd2
cherkasDev <cherkasyauheni.poland@gmail.com>
2021-09-14 22:20:31 +0200

    feat: add carbon and cats tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,8 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample( sampleActivity ) {
+  let log = 0.693;
+  if( typeof(sampleActivity)!=='string' || +sampleActivity<0 || !+sampleActivity || sampleActivity > 15) return false
+  return  Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/(log/HALF_LIFE_PERIOD))
 }
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,11 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function countCats( m ) {
+  let sum = 0 ;
+  m.forEach( arr => {
+    arr.forEach(el=> el === '^^'? sum+=1 : null)
+  })
+  return sum
 }
